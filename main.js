cb7d6ad83814176819c8a23c02599aa77a33ce68
Aaron Miller <millerac333@gmail.com>
2018-06-04 10:29:39 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Use JavaScript to obtain a reference to the first article header and change its text with textContent property to "Welcome the {insert your name here} blog"
+// Use JavaScript to obtain a reference to all article__header elements and change their classList property value to "article__header important".
+
+const oldSectionHeader = document.querySelector(".article__header");
+
+oldSectionHeader.textContent = "Welcome Aaron blog";
+
+
+
+
+
